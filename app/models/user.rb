e41d3ea4d4bdaf9f748068d8a24988a46aa00bec
Alvin Taveras <alvintaveras@gmail.com>
2014-07-31 17:50:08 -0400

    sso

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :registerable, :cas_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable #:database_authenticatable
+
+    def cas_extra_attributes=(extra_attributes)
+    	extra_attributes.each do |name, value|
+    case name.to_sym
+     		when :fullname
+        		self.fullname = value
+      		when :email
+        		self.email = value
+      		end
+    	end
+  	end
 end
