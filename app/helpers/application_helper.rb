2de476bba91552fdabd7185f2ff9197fc9df1876
Alvin Taveras <alvintaveras@gmail.com>
2014-08-01 14:11:31 -0400

    some style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,18 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  # def page_id
+  #   if id = content_for(:body_id) and id.present?
+  #     return id
+  #   else
+  #     base = controller.class.to_s.gsub("Controller", '').underscore.gsub("/", '_')
+  #     return "#{base}-#{controller.action_name}"
+  #   end
+  # end
+  
+  # def page_class
+  #   controller.class.to_s.gsub("Controller", '').underscore.gsub("/", '_')+" "+content_for(:page_class)
+  # end
+
 end
